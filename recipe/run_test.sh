a2e5a772017853f30b5a080c8a73319d358948e6
Lion Krischer <lion.krischer@gmail.com>
2017-04-26 11:06:05 +0200

    Generically up ulimit on OSX.

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,5 +1,5 @@
-# Up ulimit on Travis and OSX.
-if [[ $(uname) == "Darwin" ]] && [[ $TRAVIS == true ]] && [[ $(ulimit -n) -lt 4096 ]] ; then
+# Up ulimit on OSX.
+if [[ $(uname) == "Darwin" ]] && [[ $(ulimit -n) -lt 4096 ]] ; then
     ulimit -n 4096
 fi
 echo "ulimit -n:" `ulimit -n`
